356690e926e31b3e30451b244e760b0642b2795e
Irena Omelana <124605706+IrenOm@users.noreply.github.com>
2023-05-23 15:24:42 +0300

    Delete All button Created

--- HealthNewsFeed/Controller/SavedTableViewController.swift
+++ HealthNewsFeed/Controller/SavedTableViewController.swift
@@ -59,99 +59,111 @@ class SavedTableViewController: UITableViewController {
         loadCoreData()
     }
     
-    
-    
-    // MARK: - Table view data source
-    
-    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        //        return savedItems.count
+    func deleteAllCoreData(){
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Items")
         
-        if savedItems.count == 0 {
-            tableView.setEmptyView(title: "You don't have any saved contact.", message: "Plesae go to News Tab Bar to check the articles.")
-        }
-        else {
-            tableView.restore()
-        }
-        return savedItems.count
-        
-    }
-    
-    
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "savedCell", for: indexPath) as? NewsTableViewCell else { return UITableViewCell() }
-        
-        let item = savedItems[indexPath.row]
-        cell.newsTitleLabel.text = item.newsTitle ?? ""
-        cell.newsTitleLabel.numberOfLines = 0
-        cell.newsImageView.sd_setImage(with: URL(string: item.image ?? ""))
-        cell.selectionStyle = .none
-        return cell
-    }
-    
-    override func  tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 100
-    }
-    
-    
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
+        let entityRequest: NSBatchDeleteRequest = NSBatchDeleteRequest(fetchRequest: fetchRequest)
+        do {
+            try context?.execute(entityRequest)
+            basicAlert(title: "Deleted!", message: "Ooops, I just delete your article from Core Data.")
+            saveCoreData()
+        }catch let error{
+            print(error.localizedDescription)
             
-            let alert = UIAlertController(title: "Delete", message: "Are you sure you want to delete?", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
-            alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { _ in
-                let item = self.savedItems[indexPath.row]
-                self.context?.delete(item)
-                self.saveCoreData()
-            }))
-            self.present(alert, animated: true)
+        }
+        
+        // MARK: - Table view data source
+        
+        override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+            // #warning Incomplete implementation, return the number of rows
+            //        return savedItems.count
+            
+            if savedItems.count == 0 {
+                tableView.setEmptyView(title: "You don't have any saved contact.", message: "Plesae go to News Tab Bar to check the articles.")
+            }
+            else {
+                tableView.restore()
+            }
+            return savedItems.count
+            
+        }
+        
+        
+        override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "savedCell", for: indexPath) as? NewsTableViewCell else { return UITableViewCell() }
+            
+            let item = savedItems[indexPath.row]
+            cell.newsTitleLabel.text = item.newsTitle ?? ""
+            cell.newsTitleLabel.numberOfLines = 0
+            cell.newsImageView.sd_setImage(with: URL(string: item.image ?? ""))
+            cell.selectionStyle = .none
+            return cell
+        }
+        
+        override func  tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+            return 100
+        }
+        
+        
+        // Override to support editing the table view.
+        override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+            if editingStyle == .delete {
+                
+                let alert = UIAlertController(title: "Delete", message: "Are you sure you want to delete?", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Cancel", style: .cancel))
+                alert.addAction(UIAlertAction(title: "Delete", style: .destructive, handler: { _ in
+                    let item = self.savedItems[indexPath.row]
+                    self.context?.delete(item)
+                    self.saveCoreData()
+                }))
+                self.present(alert, animated: true)
+            }
+        }
+        
+        
+        override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+            let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
+            
+            guard let vc = storyboard.instantiateViewController(withIdentifier: "WebViewController") as? WebViewController else { return }
+            
+            vc.urlString = savedItems[indexPath.row].url ?? "https://www.healthline.com/health-news"
+            navigationController?.pushViewController(vc, animated: true)
+            
         }
     }
     
+    extension UITableView {
+        func setEmptyView(title: String, message: String) {
+            let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
+            
+            let titleLabel = UILabel()
+            let messageLabel = UILabel()
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            messageLabel.translatesAutoresizingMaskIntoConstraints = false
+            titleLabel.textColor = UIColor.black
+            titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
+            messageLabel.textColor = UIColor.lightGray
+            messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
+            emptyView.addSubview(titleLabel)
+            emptyView.addSubview(messageLabel)
+            titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
+            titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
+            messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
+            messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
+            messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
+            titleLabel.text = title
+            messageLabel.text = message
+            messageLabel.numberOfLines = 0
+            messageLabel.textAlignment = .center
+            
+            // The only tricky part is here:
+            self.backgroundView = emptyView
+            self.separatorStyle = .none
+        }
+        func restore() {
+            self.backgroundView = nil
+            self.separatorStyle = .singleLine
+        }
+    }
     
-    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let storyboard = UIStoryboard(name: "Main", bundle: Bundle.main)
-        
-        guard let vc = storyboard.instantiateViewController(withIdentifier: "WebViewController") as? WebViewController else { return }
-        
-        vc.urlString = savedItems[indexPath.row].url ?? "https://www.healthline.com/health-news"
-        navigationController?.pushViewController(vc, animated: true)
-        
-    }
 }
-
-extension UITableView {
-    func setEmptyView(title: String, message: String) {
-        let emptyView = UIView(frame: CGRect(x: self.center.x, y: self.center.y, width: self.bounds.size.width, height: self.bounds.size.height))
-        
-        let titleLabel = UILabel()
-        let messageLabel = UILabel()
-        titleLabel.translatesAutoresizingMaskIntoConstraints = false
-        messageLabel.translatesAutoresizingMaskIntoConstraints = false
-        titleLabel.textColor = UIColor.black
-        titleLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 18)
-        messageLabel.textColor = UIColor.lightGray
-        messageLabel.font = UIFont(name: "HelveticaNeue-Regular", size: 17)
-        emptyView.addSubview(titleLabel)
-        emptyView.addSubview(messageLabel)
-        titleLabel.centerYAnchor.constraint(equalTo: emptyView.centerYAnchor).isActive = true
-        titleLabel.centerXAnchor.constraint(equalTo: emptyView.centerXAnchor).isActive = true
-        messageLabel.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 20).isActive = true
-        messageLabel.leftAnchor.constraint(equalTo: emptyView.leftAnchor, constant: 20).isActive = true
-        messageLabel.rightAnchor.constraint(equalTo: emptyView.rightAnchor, constant: -20).isActive = true
-        titleLabel.text = title
-        messageLabel.text = message
-        messageLabel.numberOfLines = 0
-        messageLabel.textAlignment = .center
-        
-        // The only tricky part is here:
-        self.backgroundView = emptyView
-        self.separatorStyle = .none
-    }
-    func restore() {
-        self.backgroundView = nil
-        self.separatorStyle = .singleLine
-    }
-}
-
